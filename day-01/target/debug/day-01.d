79ad02834905cf5ca4c65c03c4c93d1e1829aed8
Frederico Wieser <20886677+fredericowieser@users.noreply.github.com>
2023-12-02 00:20:08 +0000

    Remove target folder

--- day-01/target/debug/day-01.d
+++
@@ -1,1 +0,0 @@
-/Users/fredericowieser/flow/_projects/Advent-Of-Code/day-01/target/debug/day-01: /Users/fredericowieser/flow/_projects/Advent-Of-Code/day-01/src/main.rs
--- day-01/target/debug/deps/day_01-a1a55da3c4d9107a.d
+++
@@ -1,5 +0,0 @@
-/Users/fredericowieser/flow/_projects/Advent-Of-Code/day-01/target/debug/deps/day_01-a1a55da3c4d9107a: src/main.rs
-
-/Users/fredericowieser/flow/_projects/Advent-Of-Code/day-01/target/debug/deps/day_01-a1a55da3c4d9107a.d: src/main.rs
-
-src/main.rs:
