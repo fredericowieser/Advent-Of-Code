dbe549c680e168f562bbcd33f4151ddc962ddeae
Frederico Wieser <20886677+fredericowieser@users.noreply.github.com>
2023-12-02 00:10:54 +0000

    Add day-01 skeleton

---
+++ day-01/target/debug/day-01.d
@@ -0,0 +1,1 @@
+/Users/fredericowieser/flow/_projects/Advent-Of-Code/day-01/target/debug/day-01: /Users/fredericowieser/flow/_projects/Advent-Of-Code/day-01/src/main.rs
---
+++ day-01/target/debug/deps/day_01-a1a55da3c4d9107a.d
@@ -0,0 +1,5 @@
+/Users/fredericowieser/flow/_projects/Advent-Of-Code/day-01/target/debug/deps/day_01-a1a55da3c4d9107a: src/main.rs
+
+/Users/fredericowieser/flow/_projects/Advent-Of-Code/day-01/target/debug/deps/day_01-a1a55da3c4d9107a.d: src/main.rs
+
+src/main.rs:
